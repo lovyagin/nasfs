366581ac2894e0b382059d1a09f6722214881a45
Morozov Nikita <nikitwww@gmail.com>
2025-06-07 22:54:34 +0300

    feat(connections): add GET <filepath> command

--- server/src/network/connections.c
+++ server/src/network/connections.c
@@ -4,13 +4,16 @@
    This file is part of NASFS server.
    Handles client socket connections and implements the echo protocol.  */
 
+#include <_string.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/syslimits.h>
 #include <unistd.h>
 
 #include "logging/log.h"
@@ -19,6 +22,9 @@
 
 /* Size of the buffer used for receiving client data.  */
 #define BUFFER_SIZE 1024
+
+/* USER COMMANDS MACROSES.  */
+#define GET_COMMAND "GET"
 
 /* Handle a client connection in a separate thread.
    Accepts a client socket descriptor via CLIENT_SOCKET_PTR (which is freed),
@@ -64,9 +70,63 @@ handle_client (void *client_socket_ptr)
       buffer[bytes_received] = '\0';
       log_all (LOG_DEBUG, "Received from client: '%s'", buffer);
 
-      /* Echo the received data back to client */
-      send (client_sock, buffer, bytes_received, 0);
-      log_all (LOG_INFO, "Response sent");
+      char *command = strtok (buffer, " \t\n");
+      if (command)
+        {
+          if (strcmp (command, "GET") == 0)
+            {
+              char *filepath = strtok (NULL, " \t\n\r");
+              log_all (LOG_DEBUG, "%s", filepath);
+              if (filepath)
+                {
+                  log_all (LOG_INFO, "Client requested file: %s", filepath);
+                  FILE *file = fopen (filepath, "rb");
+
+                  if (file)
+                    {
+                      log_all (LOG_INFO, "Sending file: %s", filepath);
+
+                      const char *response_ok = "OK\n";
+                      send (client_sock, response_ok, strlen (response_ok), 0);
+
+                      char file_buffer[BUFFER_SIZE];
+                      size_t bytes_read;
+                      while ((bytes_read = fread (file_buffer, 1,
+                                                  sizeof (file_buffer), file))
+                             > 0)
+                        {
+                          send (client_sock, file_buffer, bytes_read, 0);
+                        }
+                      fclose (file);
+                      log_all (LOG_INFO, "Finished sending file: %s",
+                               filepath);
+                    }
+                  else
+                    {
+                      log_all (LOG_ERROR, "Failed to open file: %s", filepath);
+                      const char *response_error
+                          = "ERROR: Failed to open file\n";
+                      send (client_sock, response_error,
+                            strlen (response_error), 0);
+                    }
+                }
+              else
+                {
+                  log_all (LOG_WARNING,
+                           "GET command received without filepath");
+                  const char *response_error = "ERROR: No filepath provided\n";
+                  send (client_sock, response_error, strlen (response_error),
+                        0);
+                }
+            }
+          else
+            {
+
+              log_all (LOG_WARNING, "Unknown command received: %s", command);
+              const char *response_error = "ERROR: Unknown command\n";
+              send (client_sock, response_error, strlen (response_error), 0);
+            }
+        }
     }
 
   close (client_sock);
